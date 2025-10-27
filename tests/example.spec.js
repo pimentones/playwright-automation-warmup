67779d883faa60bda988d4ab8aef9c7623c70e6b
pedropimenta <ppimenta@gmail.com>
2025-10-27 21:59:14 +0000

    playwright warmup mindera 1st day

---
+++ tests/example.spec.js
@@ -0,0 +1,23 @@
+// @ts-check
+import { test, expect } from "@playwright/test";
+
+test("has title", async ({ page }) => {
+  await page.goto("https://playwright.dev/");
+
+  // Expect a title "to contain" a substring.
+  // await by default waits 5s for the condition to be met.
+  // we need to use async in conjuntion with await due to js not being synchronous
+  await expect(page).toHaveTitle(/Playwright/);
+});
+
+test("get started link", async ({ page }) => {
+  await page.goto("https://playwright.dev/");
+
+  // Click the get started link.
+  await page.getByRole("link", { name: "Get started" }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(
+    page.getByRole("heading", { name: "Installation" })
+  ).toBeVisible();
+});
