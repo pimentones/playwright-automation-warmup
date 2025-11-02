d73f8f7c1e1b9fe8062d7561d3670b3def718e21
pedropimenta <ppimenta@gmail.com>
2025-11-02 19:24:30 +0000

    use dotenv environment variables

--- playwright.config.js
+++ playwright.config.js
@@ -1,13 +1,17 @@
 // @ts-check
 import { defineConfig, devices } from "@playwright/test";
+import dotenv from "dotenv";
 
 /**
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
+
+// default configs
+// import path from "path";
+// dotenv.config({ path: path.resolve(__dirname, ".env") });
+
+dotenv.config;
 
 /**
  * @see https://playwright.dev/docs/test-configuration
@@ -27,7 +31,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    baseURL: "https://playground-drab-six.vercel.app/",
+    baseURL: process.env.BASEURL,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
@@ -77,4 +81,6 @@ export default defineConfig({
   //   url: 'http://localhost:3000',
   //   reuseExistingServer: !process.env.CI,
   // },
+
+  /* Run your local dev server before starting the tests */
 });
