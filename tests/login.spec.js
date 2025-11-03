ccaf0e223d227d3bdbca98150f7ab7e8a4537ffd
pedropimenta <ppimenta@gmail.com>
2025-11-03 20:31:54 +0000

    wip

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -12,7 +12,6 @@ test.describe("Authentication tests", { tag: "@autehntication" }, () => {
     });
   });
 
-  // test using dotenv credentials
   test("Successful login", async ({ page }) => {
     await page
       .getByRole("textbox", { name: "Type your username" })
@@ -23,18 +22,6 @@ test.describe("Authentication tests", { tag: "@autehntication" }, () => {
     await page.getByRole("button", { name: "Login" }).click();
     await expect(page.getByText("User successfully logged in!")).toBeVisible();
   });
-
-  // Bruno's code for the same test
-  // import test, { expect } from "@playwright/test";
-  // test('Login sucessful', async ({page})=>{
-
-  //     await page.goto('https://playground-drab-six.vercel.app/login');
-  //     await page.getByRole('textbox', {name: 'Type your username'}).fill('test');
-  //     await page.getByRole('textbox', {name: 'Type your password'}).fill('password123');
-  //     await page.getByRole('button', { name: 'Login' }).click();
-
-  //     await expect(page.getByText('User successfully logged in!')).toBeVisible();
-  // })
 
   test("Blocked account", async ({ page }) => {
     await page
@@ -79,9 +66,9 @@ test.describe("Authentication tests", { tag: "@autehntication" }, () => {
       .getByRole("textbox", { name: "Type your password" })
       .fill(users.wrongPasswordUser.password);
     await test.step("Attempting login with invalid credentials 3 times until blocking the account", async () => {
-      await page.getByRole("button", { name: "Login" }).click();
-      await page.getByRole("button", { name: "Login" }).click();
-      await page.getByRole("button", { name: "Login" }).click();
+      await page
+        .getByRole("button", { name: "Login" })
+        .click({ clickCount: 3 });
       await expect(page.getByText("User temporarily blocked!")).toBeVisible();
     });
   });
